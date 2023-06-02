ddbe94bd3d6a50f56b2f24f389c0593cc96afbcb
Nguyễn Duy Khánh <khanh@MacBook-Air-cua-Nguyen.local>
2023-06-02 14:14:15 +0700

    noti error when login wrong pass and mail

--- src/controller/handle/loginController.js
+++ src/controller/handle/loginController.js
@@ -21,8 +21,16 @@ class LoginController {
             })
             req.on("end", async () => {
                 let userInfo = qs.parse(data);
-                await loginService.login(userInfo.username, userInfo.password) ? res.writeHead(301, {"location": "/home"}) : res.writeHead(301, {"location": "/signin"});
-                res.end();
+                if (await loginService.login(userInfo.username, userInfo.password)) {
+                    res.writeHead(301, {"location": "/home"})
+                } else {
+                    fs.readFile('./src/view/login_logout/login.html', "utf-8", (err, data)=> {
+                        if (err) throw err.message;
+                        data = data.replace('id="retry">', 'id="retry">' + "Email or password is not right");
+                        res.write(data);
+                        res.end();
+                    })
+                }
             })
         }
     }
