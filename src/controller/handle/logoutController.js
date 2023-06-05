947113c674bfbf59d0b8ebc1c8a201fbc811ec84
NDKhanh1996 <ndkhanh1996@gmail.com>
2023-06-05 13:49:00 +0700

    fix logout complete

--- src/controller/handle/logoutController.js
+++ src/controller/handle/logoutController.js
@@ -3,9 +3,10 @@ const fs = require('fs')
 class LogoutController {
     logout(req, res) {
         if (req.url === '/logout') {
-            fs.writeFile('./src/session/session.txt', '', (err) => {
+            fs.unlink('./src/session/session.txt', (err) => {
                 if (err) throw err.message;
-                res.writeHead(301, {"location": "/home"});
+                res.setHeader('Cache-Control','no-cache, no-store, must-revalidate')
+                res.writeHead(301, {"location": "/login"});
                 res.end();
             })
         }
